17613a2c291773888a386f864add64a779ee94aa
Marce HB <anmarphy@gmail.com>
2023-03-27 13:29:39 -0500

    Adding some int Collections functions

--- Day_60/main.py
+++ Day_60/main.py
@@ -51,3 +51,67 @@ print(account[0])
 Account = namedtuple('Accounts', ['name', 'balance'])
 account = Account('checking', 3234) 
 print(account)
+
+##deque: Double ended queue
+
+friends = deque(('Rolf', 'Charlie', 'Anna'))
+friends.append('Jose')
+friends.appendleft('Anthony')
+print(friends)
+
+
+#### Exercise 19
+def task1() -> defaultdict:
+    """
+    - create a `defaultdict` object, and its default value would be set to the string `Unknown`.
+    - Add an entry with key name `Alan` and its value being `Manchester`.
+    - Return the `defaultdict` object you created.
+    """
+    # you code starts here:
+    data = defaultdict(lambda: 'Unknown')
+    data['Alan'] = 'Manchester'
+    return data
+
+print(task1())
+
+
+def task2(arg_od: OrderedDict):
+    """
+    - takes in an OrderedDict `arg_od`
+    - Remove the first and last entry in `arg_od`.
+    - Move the entry with key name `Bob` to the end of `arg_od`.
+    - Move the entry with key name `Dan` to the start of `arg_od`.
+    - You may assume that `arg_od` would always contain the keys `Bob` and `Dan`,
+        and they won't be the first or last entry initially.
+    """
+    # you code starts here:
+    arg_od.popitem()
+    arg_od.popitem(False)
+    # remember to remove start and end before moving Bob and Dan, otherwise they will be removed instead
+    arg_od.move_to_end('Bob')
+    arg_od.move_to_end('Dan', False)
+
+
+def task3(name: str, club: str) -> namedtuple:
+    """
+    - create a `namedtuple` with type `Player`, and it will have two fields, `name` and `club`.
+    - create a `Player` `namedtuple` instance that has the `name` and `club` field set by the given arguments.
+    - return the `Player` `namedtuple` instance you created.
+    """
+    # you code starts here:
+    Player = namedtuple('Player', ['name', 'club'])
+    player = Player(name, club)
+    return player
+
+
+def task4(arg_deque: deque):
+    """
+    - Manipulate the `arg_deque` in any order you preferred to achieve the following effect:
+        -- remove last element in `deque`
+        -- move the fist (left most) element to the end (right most)
+        -- add an element `Zack`, a string, to the start (left)
+    """
+    # you code starts here:
+    arg_deque.pop()
+    arg_deque.append(dq.popleft()) 
+    arg_deque.appendleft('Zack')
