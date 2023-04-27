4e88f7b6c77a5fec2534e3a2ff727699d57e5d9a
Marce HB <anmarphy@gmail.com>
2023-04-27 11:11:24 -0500

    Adding *args and **kwargs

--- Day_67/main.py
+++ Day_67/main.py
@@ -1,1 +1,21 @@
+def add_all(a1, a2, a3, a4):
+    return a1+a2+a3+a4
 
+vals = (1,3,4,5)
+print(add_all(*vals))
+
+vals = {'a1' : 1, 'a2':3, 'a3':4, 'a4':5}
+print(add_all(**vals))
+
+def add_all(*args):
+    return sum(args)
+
+print(add_all(1,5,6,4))
+
+def pretty_print(**kwargs):
+    for k, v in kwargs.items():
+        print(f'For {k} we have {v}')
+
+print(pretty_print(user ='Marce', access_level='user'))
+
+pretty_print(**{'day':'Thursday', 'class':'aerial'})
